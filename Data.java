17f48d0f81e842f46e98449b273ac3e38b1d0456
alshenaifi <33229913+alshenaifi@users.noreply.github.com>
2017-11-06 12:16:44 -0500

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,25 @@
+class Data {
+    
+    private String CourseName;
+    private int TotalHours;
+    private int CompltedHours;
+    
+    public String getCourseName() {
+       return CourseName;
+    }
+    public void setCourseName(String CourseName) {
+       this.CourseName = CourseName;
+    }
+    public int getTotalHours() {
+       return TotalHours;
+    }
+    public void setTotalHours(int TotalHours) {
+       this.TotalHours = TotalHours;
+    }
+    public int getCompltedHours() {
+       return CompltedHours;
+    }
+    public void setCompltedHours(int CompltedHours) {
+       this.CompltedHours = CompltedHours;
+    }
+}
